8638a9eb2a07d17d6264b04c977740d9132ef623
disruption <disruption@gandhithedergrawr.com>
2023-09-19 15:43:14 +0200

    RAWR!

---
+++ app/src/androidTest/java/io/github/dkter/rawr/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package io.github.dkter.rawr
+
+import androidx.test.platform.app.InstrumentationRegistry
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("io.github.dkter.aaaaa", appContext.packageName)
+    }
+}
